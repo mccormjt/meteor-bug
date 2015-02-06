d4527ce22f46d851dca5514b7039fa885dc434df
Isaac Sanders <isaac@isaacbfsanders.com>
2015-02-06 16:28:00 -0500

    Added a statusbar on mobile

---
+++ mobile-config.js
@@ -0,0 +1,11 @@
+App.info({
+  id: 'fm.crowdplay.app',
+  name: 'CrowdPlay',
+  description: '<tag line>',
+  author: 'CrowdPlay Development',
+  email: 'crowdplay@gmail.com',
+  website: 'http://crowdplay.fm'
+});
+
+App.setPreference('StatusBarOverlaysWebView', false);
+App.setPreference('StatusBarBackgroundColor', '#000000');
