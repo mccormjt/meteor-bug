2529aee51af679a1abf80cf04c94f68ba1e7b4d2
John McCormack <mccormjt@gmail.com>
2015-04-01 02:40:43 -0400

    NEXT 22

--- server/publications.js
+++ server/publications.js
@@ -2,14 +2,6 @@ Meteor.publish('lock', function() {
     return Lock.find();
 });
 
-Meteor.publish('songQueue', function(cloudId) {
-    return Songs.find({ cloudId: cloudId });
-});
-
 Meteor.publish('users', function(cloudId) {
     return Meteor.users.find({ cloudId: cloudId });
-});
-
-Meteor.publish('cloudUsers', function(cloudId) {
-    return CloudUsers.find({ cloudId: cloudId });
 });
